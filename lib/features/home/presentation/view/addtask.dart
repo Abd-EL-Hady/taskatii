a5030ae801b9d3ac4aef1d35158093725e1e3e6a
Abd-EL-Hady <100303981+Abd-EL-Hady@users.noreply.github.com>
2024-03-17 14:08:31 +0200

    init

--- lib/features/home/presentation/view/addtask.dart
+++ lib/features/home/presentation/view/addtask.dart
@@ -15,94 +15,91 @@ class Add_task extends StatefulWidget {
 class _Add_taskState extends State<Add_task> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.arrow_back_ios,
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.arrow_back_ios,
           ),
-          title: const Center(child: Text('Add Task')),
         ),
-        body: const Padding(
-          padding: EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Title',
-                  style: TextStyle(fontSize: 15),
-                ),
-                Gap(10),
-                textform(
-                  hinttext: 'Enter Title Here',
-                ),
-                Gap(10),
-                Text(
-                  'Note',
-                  style: TextStyle(fontSize: 15),
-                ),
-                Gap(10),
-                textform(
-                  hinttext: 'Enter Note Here',
-                ),
-                Gap(10),
-                Text(
-                  'Date',
-                  style: TextStyle(fontSize: 15),
-                ),
-                Gap(10),
-                textform(
-                  icon: Icons.calendar_month_sharp,
-                  hinttext: '14/3/2024',
-                ),
-                Gap(10),
-                Padding(
-                  padding: EdgeInsets.only(left: 10),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Start Time',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Gap(100),
-                      Text(
-                        'End Time',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ],
+        title: const Center(child: Text('Add Task')),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Title',
+                style: TextStyle(fontSize: 15),
+              ),
+              Gap(10),
+              textform(
+                hinttext: 'Enter Title Here',
+              ),
+              Gap(10),
+              Text(
+                'Note',
+                style: TextStyle(fontSize: 15),
+              ),
+              Gap(10),
+              textform(
+                hinttext: 'Enter Note Here',
+              ),
+              Gap(10),
+              Text(
+                'Date',
+                style: TextStyle(fontSize: 15),
+              ),
+              Gap(10),
+              textform(
+                icon: Icons.calendar_month_sharp,
+                hinttext: '14/3/2024',
+              ),
+              Gap(10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Start Time',
+                      style: TextStyle(fontSize: 15),
+                    ),
                   ),
-                ),
-                Gap(10),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gap(10),
-                    Flexible(
-                        child: textform(
-                      hinttext: '02:00 AM',
-                      icon: Icons.update,
-                    )),
-                    Gap(10),
-                    Flexible(
-                        child: textform(
-                      hinttext: '02:00 AM',
-                      icon: Icons.update,
-                    )),
-                  ],
-                ),
-                Row(
-                  children: [
-            
-                  ],
-                )
-              ],
-            ),
+                  Gap(10),
+                  Expanded(
+                    child: Text(
+                      'End Time',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ),
+                ],
+              ),
+              Gap(10),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Gap(10),
+                  Flexible(
+                    fit: FlexFit.loose,
+                      child: textform(
+                    hinttext: '02:00 AM',
+                    icon: Icons.update,
+                  )),
+                  Gap(10),
+                  Flexible(
+                      child: textform(
+                    hinttext: '02:00 AM',
+                    icon: Icons.update,
+                  )),
+                ],
+              ),
+              Row(
+                children: [],
+              )
+            ],
           ),
         ),
       ),
