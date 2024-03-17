bf9dc30365afd704ea242661be510f84697ebf90
Abd-EL-Hady <100303981+Abd-EL-Hady@users.noreply.github.com>
2024-03-17 16:20:19 +0200

    init

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,37 +1,83 @@
+import 'package:date_picker_timeline/date_picker_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:taskati_3_4/features/home/presentation/widgets/home_header.dart';
+import 'package:gap/gap.dart';
 
+import '../widgets/home_header.dart';
 import 'addtask.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
-
   @override
   State<HomeView> createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Padding(
           padding: const EdgeInsets.all(15),
-          child: Column(
-            children: [
-              // Header
-              const HomeHeader(),
-              FloatingActionButton.large(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const Add_task()),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                // Header
+                const HomeHeader(),
+                const Gap(15),
+                Row(
+                  children: [
+                    const Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('March , 17, 2024'),
+                          Gap(10),
+                          Text('Today'),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: FloatingActionButton.extended(
+                        backgroundColor: Colors.blue[900],
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Add_task()),
+                          );
+                        },
+                        label: const Text(
+                          ' + Add Task',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 100,
+                  child: DatePicker(
+                    width: 80,
+                    DateTime.now(),
+                    initialSelectedDate: DateTime.now(),
+                    selectionColor: Colors.blue.shade900,
+                    selectedTextColor: Colors.white,
+                    onDateChange: (date) {
+                      // New date selected
+                      setState(() {});
+                    },
+                  ),
+                ),
+                ListView.builder(itemBuilder: (context, int index){
+                  return const ListTile(
+
                   );
-                },
-                child: const Text('Add Task'),
-              )
-            ],
+                }),
+
+              ],
+            ),
           ),
         ),
       ),
